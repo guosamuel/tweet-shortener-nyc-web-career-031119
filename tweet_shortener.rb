6750940f47fb58f926b4dbe9d123136e7d7bfbb9
Samuel Guo <guosamuel1114@gmail.com>
2019-02-28 13:59:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,6 +32,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(array_of_tweets)
+  binding.pry
   array_of_tweets.each do |tweet_string|
     puts word_substituter(tweet_string.join(" "))
   end
