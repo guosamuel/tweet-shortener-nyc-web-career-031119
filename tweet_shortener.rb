e70c962823bf1d1bb451ec75096b700b1495fc23
Samuel Guo <guosamuel1114@gmail.com>
2019-02-28 13:43:37 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -24,7 +24,7 @@ def word_substituter(tweet)
     tweet_array.each do |word|
       if word == keys_array[counter]
         tweet_array[word.index] = dictionary[keys_array[counter]]
-        binding,pry
+        binding.pry
       end
     end
     counter += 1
