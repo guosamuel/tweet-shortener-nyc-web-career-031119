d1c9b428eecec0b44f66d9fec94165b8723cc5d2
Samuel Guo <guosamuel1114@gmail.com>
2019-02-28 14:11:53 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -48,5 +48,9 @@ def bulk_tweet_shortener(array_of_tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  
+  if tweet.length > 140
+    puts word_substituter(tweet)
+  else
+    puts tweet
+  end
 end
